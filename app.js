1ada4d608d54bd9292d45732df9afe57c65c9957
jun0605odk <jun0605odk@gmail.com>
2020-07-11 02:38:28 +0900

    ver2.0 commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -6,12 +7,40 @@ var port = process.env.PORT || 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  var home_url = "http://localhost:" + port;
+// middleware
+app.use(express.static(__dirname + '/static')) //静的ファイルを使用する
 
+// ルーティング
+var home_url = "http://localhost:" + port;
+
+app.get('/', function(req, res){
   res.render('index', {home_url:home_url});
-  // res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/sign-up', function(req, res){
+  res.render('sign-up', {home_url:home_url});
+});
+
+app.get('/log-in', function(req, res){
+  res.render('log-in', {home_url:home_url});
+});
+
+app.get('/room', function(req, res){
+  res.render('room-select', {home_url:home_url});
+});
+
+app.get('/room-make', function(req, res){
+  res.render('room-make', {home_url:home_url});
+});
+
+app.get('/room/:room_name', function(req, res){
+  var home_url = "http://localhost:" + port;
+  res.render('room', {
+    home_url:home_url,
+    room_name:req.params.room_name
+  });
+});
+
 
 var chat = io.of('/chat');
 chat.on('connection', function(socket){
